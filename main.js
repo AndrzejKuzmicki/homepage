d7a8bd2c403317a30a20a8a287ea5beb949d3724
Andrzej Kuzmicki <kuzmicki.a@gmail.com>
2020-05-10 23:00:56 +0200

    add picture in javascript

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const firstName = 'Andrzej';
 
 console.log(`Cześć! Jestem ${firstName}. Witam Cię na mojej stronie :)`)
+
+const image = document.querySelector('.notes__picture--js')
+
+image.innerHTML = `<img src="wtf-wpidea.png" alt="wtf, co ten frontend">`
