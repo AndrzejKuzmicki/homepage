02ed52612148605a5b2c8c7517afa3a179e1c7f3
AndrzejKuzmicki <kuzmicki.a@gmail.com>
2023-08-18 14:16:37 +0200

    firstFunction

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const age = 41;
 
 
 console.log(`Cześć, nazywam się ${firstName} i mam ${age} lat! Kałabanga i do przodu!`);
+
+const header = document.querySelector('h1');
+
+console.log(header);
+
+const headerDescription = document.querySelector('.header__description--js')
+
+headerDescription.innerHTML = 'Moja pierwsza strona internetowa'
+
+const hello = () => {
+    console.log(`Cześć, nazywam się ${firstName} i mam ${age} lat! Kałabanga i do przodu!`);
+}
+
+hello ();
